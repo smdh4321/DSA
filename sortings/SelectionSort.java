b2a59a46028de63ae26e24c37d16d60453f3db65
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-04-11 17:51:27 +0530

    Selection Sort

--- sortings/SelectionSort.java
+++ sortings/SelectionSort.java
@@ -1,4 +1,24 @@
 package sortings;
 
 public class SelectionSort {
+    static void selectionSort(int[] arr){
+        for(int i = 0; i< arr.length; i++){
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[min] > arr[j]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {13,46,24,52,20,9};
+        selectionSort(arr);
+    }
 }
