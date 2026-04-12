021e661b910cf1081f1b3dce412cbc2f07a9dfec
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-04-12 16:06:05 +0530

    Insertion Sort

---
+++ sortings/InsertionSort.java
@@ -0,0 +1,22 @@
+package sortings;
+
+public class InsertionSort {
+    static void insertionSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            int j = i;
+            while(j > 0 && arr[j - 1] > arr[j]){
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {8,1,4,3,9,5};
+        insertionSort(arr);
+    }
+}
