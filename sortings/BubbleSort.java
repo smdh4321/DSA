3d5bd10613b87d8b832e7a24f16da09cb4649aa6
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-04-12 15:47:16 +0530

    Optimized Bubble Sort

--- sortings/BubbleSort.java
+++ sortings/BubbleSort.java
@@ -3,12 +3,17 @@ package sortings;
 public class BubbleSort {
     static void bubbleSort(int[] arr){
         for(int i = 0; i < arr.length; i++){
+            int isSwap = 0;
             for(int j = 0; j < arr.length - i - 1; j++){
                 if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    isSwap = 1;
                 }
+            }
+            if(isSwap == 0){
+                break;
             }
         }
         for(int i = 0; i < arr.length; i++){
