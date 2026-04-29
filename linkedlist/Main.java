7c95c26fd661296df2f0a78f47dab8791bbd7c0c
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-04-29 23:13:39 +0530

    Create,Add elements and printing LL

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -18,20 +18,26 @@ class LinkedList{
             curr = curr.next;
         }
     }
+    void addNode(int ele){
+        Node temp = new Node(ele);
+        if(head == null){    // check linkedlist is empty or not
+            head = temp;
+        }else{
+            Node curr = head;
+            while(curr.next != null){
+                curr = curr.next;
+            }
+            curr.next = temp;
+        }
+    }
 }
-public class CreateLinkedList {
+public class Main {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        Node p1 = new Node(10);
-        Node p2 = new Node(20);
-        Node p3 = new Node(30);
-        Node p4 = new Node(40);
-
-        p1.next = p2;
-        p2.next = p3;
-        p3.next = p4;
-
-        ll.head = p1;
+        ll.addNode(10);
+        ll.addNode(20);
+        ll.addNode(30);
+        ll.addNode(40);
         ll.printList();
     }
 }
