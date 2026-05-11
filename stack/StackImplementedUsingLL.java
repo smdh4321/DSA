0c0a64979532a5a4771496cb4155a4e5b7285c39
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-05-11 23:09:21 +0530

    Implemented size, peek, isEmpty methods in Stack

--- stack/StackImplementedUsingLL.java
+++ stack/StackImplementedUsingLL.java
@@ -10,9 +10,11 @@ class Node{
 
 class StackLL{
     Node top;
+    int size;
 
     StackLL(){
         this.top = null;
+        this.size = 0;
     }
     void push(int ele){
         Node temp = new Node(ele);
@@ -22,27 +24,45 @@ class StackLL{
             temp.next = top;
             top = temp;
         }
+        size++;
     }
 
     int pop() throws StackUnderFlowException{
         if(top == null){
             throw new StackUnderFlowException();
         }
+        size--;
         int t = top.data;
         top = top.next;
         return t;
+    }
+
+    int size(){
+        return size;
+    }
+
+    int peek(){
+        return top.data;
+    }
+
+    boolean isEmpty(){
+        if(top == null){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 public class StackImplementedUsingLL{
     public static void main(String[] args) throws Exception{
         StackLL st = new StackLL();
+        System.out.println(st.size());
         st.push(10);
         st.push(20);
         st.push(30);
         System.out.println(st.pop());
-        System.out.println(st.pop());
-        System.out.println(st.pop());
-        System.out.println(st.pop());
-
+        System.out.println(st.size());
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
     }
 }
