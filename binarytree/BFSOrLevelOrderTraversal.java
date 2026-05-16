9038abd2cc72981935a3466d28878d8eb4599c9b
Sunkara Mohammed Hussain <smdh4321@gmail.com>
2026-05-16 23:23:50 +0530

    All BFS Traversals Implemented

--- binarytree/BFSOrLevelOrderTraversal.java
+++ binarytree/BFSOrLevelOrderTraversal.java
@@ -19,8 +19,100 @@ class TreeLOT{
                     q.add(curr.right);
                 }
             }
+            System.out.println();
         }
     }
+
+    static void levelOrderTraversalApproach1(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root != null){
+            q.add(root);
+            q.add(null);
+            while(q.size() > 1){
+                Node curr = q.poll();
+                if(curr == null){
+                    System.out.println();
+                    q.add(null);
+                }else{
+                    System.out.print(curr.data + " ");
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                }
+            }
+        }
+    }
+
+    static void levelOrderTraversalApproach2(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root != null){
+            q.add(root);
+            while(q.size() > 0){
+                int count = q.size();
+                for(int i = 0; i < count; i++){
+                    Node curr = q.poll();
+                    System.out.print(curr.data + " ");
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    static void leftView(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root != null){
+            q.add(root);
+            while(q.size() > 0){
+                int count = q.size();
+                for(int i = 0;i < count; i++){
+                    Node curr = q.poll();
+                    if(i == 0){
+                        System.out.print(curr.data + " ");
+                    }
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+    static void rightView(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root != null){
+            q.add(root);
+            while(q.size() > 0){
+                int count = q.size();
+                for(int i = 0;i < count; i++){
+                    Node curr = q.poll();
+                    if(i == 0){
+                        System.out.print(curr.data + " ");
+                    }
+                    if(curr.right != null){
+                        q.add(curr.right);
+                    }
+                    if(curr.left != null){
+                        q.add(curr.left);
+                    }
+                }
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+
 }
 
 public class BFSOrLevelOrderTraversal {
@@ -34,5 +126,14 @@ public class BFSOrLevelOrderTraversal {
         root.right.right = new Node(60);
         root.right.right.left = new Node(70);
         TreeLOT.levelOrderTraversal(root);
+        System.out.println("------------------------------");
+        TreeLOT.levelOrderTraversalApproach1(root);
+        System.out.println();
+        System.out.println("------------------------------");
+        TreeLOT.levelOrderTraversalApproach2(root);
+        System.out.println("------------------------------");
+        TreeLOT.leftView(root);
+        System.out.println("------------------------------");
+        TreeLOT.rightView(root);
     }
 }
